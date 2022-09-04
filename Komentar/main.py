2a1fea08dc10260c625c147855bf950f3a2f2006
Rofi Doang <112774246+Rofid0anG@users.noreply.github.com>
2022-09-05 01:37:03 +0700

    Create main.py

---
+++ Komentar/main.py
@@ -0,0 +1,13 @@
+# Komentar dapat digunakan untuk menjelaskan kode Python.
+# Komentar dapat digunakan untuk membuat kode lebih mudah dibaca.
+# Komentar dapat digunakan untuk mencegah eksekusi saat menguji kode.
+
+# Membuat komentar
+# Komentar dimulai dengan tanda pagar (#), dan Python akan mengabaikannya.
+# Contoh:
+# Ini adalah komentar
+print("Halo, Dunia")
+
+# Komentar dapat ditempatkan diakhir baris.
+# Contoh:
+print("Halo, Rofi") # Ini adalah komentar
