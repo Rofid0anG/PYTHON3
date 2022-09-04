29f805d774e0f9d398e0566e3a74ce90871bb2a3
Rofi Doang <112774246+Rofid0anG@users.noreply.github.com>
2022-09-05 01:35:39 +0700

    Create main.py

---
+++ Variabel/main.py
@@ -0,0 +1,38 @@
+# Variabel Python
+# Variabel adalah wadah untuk menyimpan nilai data.
+
+# Membuat Variabel
+# Python tidak memiliki perintah untuk mendeklarasikan Variabel.
+# Variabel dibuat saat Anda pertama kali menetapkan nilai padanya.
+# Contoh:
+a = 14
+b = "Ucup surucup"
+print(a)
+print(b)
+
+# Variabel tidak perlu dideklarasikan dengan tipe tertentu, bahkan dapat mengubah tipe data setelah ditetapkan.
+# Contoh:
+c = 4    
+c = "Ucup surucup" 
+print(c)
+
+# Casting
+# Casting menentukan tipe data dari suatu Variabel.
+# Contoh:
+d = str(3)
+e = int(3)
+f = float(3)
+
+# Fungsi type()
+# Fungsi type() digunakan untuk mendapatkan tipe data dari suatu Variabel.
+# Contoh:
+g = 5
+h = "Ucup surucup"
+print(type(g))
+print(type(h))
+
+# Kutip tunggal atau ganda
+# Variabel string dapat dideklarasikan dengan menggunakan tanda kutip tunggal atau ganda.
+# Contoh:
+i = 'Ucup surucup'
+j = "Ucup surucup"
